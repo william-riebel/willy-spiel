f578d194c1e0c3bcb8d45ac0e9b40b7997bcd3ab
william-riebel <william.riebel@protonmail.com>
2024-07-06 21:22:39 +0200

    lua version

---
+++ lua/main.lua
@@ -0,0 +1,20 @@
+function willy(string)
+    tostring(string)
+    local last = ""
+    for i = 1, #string do
+        local current = string.sub(string, i, i) 
+        if current == last or current == string.lower(last) or current == string.upper(last) then
+            return true
+        end
+        last = current
+    end
+    return false
+end
+
+
+if type(arg[1]) == "string" then
+    io.write(tostring(willy(arg[1])))
+else
+    io.write("Mag Willy das? ")
+    io.write(tostring(willy("\n" .. io.read())))
+end
